59071fc0552222367c1d36519c16c2cca3eee26e
sbookscreator <87274938+sbookscreator@users.noreply.github.com>
2021-07-12 16:46:58 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -6,9 +6,7 @@
 <body>
 
 <img src="logo.bmp" width="347" height="124">
-Welcome <?php echo $_GET["name"]; ?><br>
-Your email address is: <?php echo $_GET["email"]; ?>
-
+Chào mừng đến với Sbooks!! <?php echo $_GET["name"]; ?><br>
 
 <html>
 <body>
