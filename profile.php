2f0a68e00843bba2c36fe3ceb77f08ed3ec43519
sbookscreator <87274938+sbookscreator@users.noreply.github.com>
2021-07-12 16:25:02 +0700

    Update and rename profile.html to profile.php

---
+++ profile.php
@@ -0,0 +1,11 @@
+<!DOCTYPE  html>
+<html>
+<body>
+
+<img src="logo.bmp" width="347" height="124">
+
+Welcome <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?>
+
+<html>
+<body>
